c5f9554ccca1d4a2c9c855a91879a30c4bb106f6
KatiaVasileva <eka.vasileva@gmail.com>
2025-08-21 21:16:36 +0500

    implement logic of encrypt in main method

--- src/main/java/com/javarush/vasileva/ConsoleRunner.java
+++ src/main/java/com/javarush/vasileva/ConsoleRunner.java
@@ -1,7 +1,62 @@
 package com.javarush.vasileva;
 
-public class ConsoleRunner {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
+public class ConsoleRunner {
+    private static final Character[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
+            'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', '«', '»', '"',
+            '\'', ':', '!', '?', ' '};
+    private static final Map<Character, Integer> mapAlphabet = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int key = scanner.nextInt();
+
+        String sourcePath = "/Users/katiavasileva/IdeaProjects/CryptoAnalyzerPantera/text/testText.txt";
+        String targetPath = "/Users/katiavasileva/IdeaProjects/CryptoAnalyzerPantera/text/testEncrypted.txt";
+
+        for (int i = 0; i < ALPHABET.length; i++) {
+            mapAlphabet.put(ALPHABET[i], i);
+        }
+
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(sourcePath));
+             BufferedWriter writer = Files.newBufferedWriter(Path.of(targetPath))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                for(char c : line.toLowerCase().toCharArray()) {
+                    int index;
+                    if (mapAlphabet.containsKey(c)) {
+                        index = mapAlphabet.get(c);
+                    } else {
+                        continue;
+                    }
+                    int indexWithKey = index + key;
+                    if (indexWithKey < mapAlphabet.size()) {
+                        writer.write(String.valueOf(getKeyByValue(mapAlphabet, indexWithKey)));
+                    } else {
+                        writer.write(String.valueOf(getKeyByValue(mapAlphabet, (index + key) % ALPHABET.length)));
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File is not found");
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+
+    public static <Character, Integer> Character getKeyByValue(Map<Character, Integer> map, Integer value) {
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (value.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 }
