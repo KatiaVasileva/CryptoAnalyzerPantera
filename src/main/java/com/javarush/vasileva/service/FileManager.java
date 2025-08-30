ffd074ae86fcb428bff38a310088c2b3c10f4b70
KatiaVasileva <eka.vasileva@gmail.com>
2025-08-30 12:15:41 +0500

    THROW exceptions to Controller and catch them

--- src/main/java/com/javarush/vasileva/service/FileManager.java
+++ src/main/java/com/javarush/vasileva/service/FileManager.java
@@ -2,7 +2,6 @@ package com.javarush.vasileva.service;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
